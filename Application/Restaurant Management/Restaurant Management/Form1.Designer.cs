05f2b3f406604832c6f78456acff9ffadcd6f0da
Dao Minh Trung Thuan <dao.mt.thuan@gmail.com>
2019-04-11 01:08:11 +0700

    create Application
    create Application with winfrom C# VS2019
    create Database with MSSQL2016

---
+++ Application/Restaurant Management/Restaurant Management/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace Restaurant_Management
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.fluentDesignFormContainer1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer();
+            this.accordionControl1 = new DevExpress.XtraBars.Navigation.AccordionControl();
+            this.accordionControlElement1 = new DevExpress.XtraBars.Navigation.AccordionControlElement();
+            this.fluentDesignFormControl1 = new DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl();
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fluentDesignFormControl1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // fluentDesignFormContainer1
+            // 
+            this.fluentDesignFormContainer1.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.fluentDesignFormContainer1.Location = new System.Drawing.Point(260, 30);
+            this.fluentDesignFormContainer1.Name = "fluentDesignFormContainer1";
+            this.fluentDesignFormContainer1.Size = new System.Drawing.Size(432, 468);
+            this.fluentDesignFormContainer1.TabIndex = 0;
+            // 
+            // accordionControl1
+            // 
+            this.accordionControl1.Dock = System.Windows.Forms.DockStyle.Left;
+            this.accordionControl1.Elements.AddRange(new DevExpress.XtraBars.Navigation.AccordionControlElement[] {
+            this.accordionControlElement1});
+            this.accordionControl1.Location = new System.Drawing.Point(0, 30);
+            this.accordionControl1.Name = "accordionControl1";
+            this.accordionControl1.ScrollBarMode = DevExpress.XtraBars.Navigation.ScrollBarMode.Touch;
+            this.accordionControl1.Size = new System.Drawing.Size(260, 468);
+            this.accordionControl1.TabIndex = 1;
+            this.accordionControl1.ViewType = DevExpress.XtraBars.Navigation.AccordionControlViewType.HamburgerMenu;
+            // 
+            // accordionControlElement1
+            // 
+            this.accordionControlElement1.Name = "accordionControlElement1";
+            this.accordionControlElement1.Text = "Element1";
+            // 
+            // fluentDesignFormControl1
+            // 
+            this.fluentDesignFormControl1.Dock = System.Windows.Forms.DockStyle.Top;
+            this.fluentDesignFormControl1.FluentDesignForm = this;
+            this.fluentDesignFormControl1.Location = new System.Drawing.Point(0, 0);
+            this.fluentDesignFormControl1.Name = "fluentDesignFormControl1";
+            this.fluentDesignFormControl1.RightToLeft = System.Windows.Forms.RightToLeft.No;
+            this.fluentDesignFormControl1.Size = new System.Drawing.Size(692, 30);
+            this.fluentDesignFormControl1.TabIndex = 2;
+            this.fluentDesignFormControl1.TabStop = false;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 20F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(692, 498);
+            this.ControlContainer = this.fluentDesignFormContainer1;
+            this.Controls.Add(this.fluentDesignFormContainer1);
+            this.Controls.Add(this.accordionControl1);
+            this.Controls.Add(this.fluentDesignFormControl1);
+            this.FluentDesignFormControl = this.fluentDesignFormControl1;
+            this.Font = new System.Drawing.Font("Segoe UI Semibold", 11.25F);
+            this.Name = "Form1";
+            this.NavigationControl = this.accordionControl1;
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.accordionControl1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.fluentDesignFormControl1)).EndInit();
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormContainer fluentDesignFormContainer1;
+        private DevExpress.XtraBars.Navigation.AccordionControl accordionControl1;
+        private DevExpress.XtraBars.Navigation.AccordionControlElement accordionControlElement1;
+        private DevExpress.XtraBars.FluentDesignSystem.FluentDesignFormControl fluentDesignFormControl1;
+    }
+}
+
---
+++ Database/SQLQuery/create.sql
@@ -0,0 +1,121 @@
+﻿use master
+--create database RestaurantManagement
+create database RestaurantManagement on primary
+(
+	Name = 'RestaurantManagement_Data',
+	Filename = 'E:\Github\restaurant-management\Database\RestaurantManagement_Data.mdf'
+)
+log on
+(
+	Name = 'RestaurantManagement_Log',
+	Filename = 'E:\Github\restaurant-management\Database\RestaurantManagement_Log.ldf'
+)
+go
+
+use RestaurantManagement
+
+-- Phân quyền tài khoản
+create table Roles
+(
+	ID int primary key,												-- Mã
+	[Name] nvarchar(100) not null									-- Loại tài khoản
+)
+go
+
+-- Trạng thái bàn
+create table TableStatus
+(
+	ID int primary key,												-- Mã
+	[Name] nvarchar(100) not null									-- Loại trạng thái
+)
+go
+
+-- Trạng thái bill
+create table BillStatus
+(
+	ID int primary key,												-- Mã
+	[Name] nvarchar(100) not null									-- Loại trạng thái
+)
+go
+
+-- Loại thức ăn
+create table FoodCategory
+(
+	ID int primary key,												-- Mã
+	[Name] nvarchar(100) not null									-- Loại thức ăn
+)
+go
+
+-- Tài khoản
+create table Accounts
+(
+	ID int identity primary key,									-- Mã
+	Username varchar(100) not null unique,							-- Tên đăng nhập
+	[Password] varchar(1000) not null default 'user',				-- Mật khẩu
+	RoleID int not null default 0,									-- Mã phân quyền
+	[Name] nvarchar(100) not null,									-- Tên
+	Gender nvarchar(3) not null,									-- Giới tính		
+	Birthday date not null,											-- Ngày sinh
+	IdentityCard  varchar(20) not null unique,						-- CMND/CCCD
+	Hometown nvarchar(100) not null,								-- Quê quán
+	[Address] nvarchar(100) not null,								-- Địa chỉ
+	Numberphone varchar(20),										-- Số điện thoại
+	Email varchar(100),												-- Email
+
+	check (Gender in ('Nam', N'Nữ')),
+	foreign key (RoleID) references Roles(ID)
+)
+go
+
+-- Bàn ăn
+create table FoodTables
+(
+	ID int identity primary key,									-- Mã
+	[Name] nvarchar(100) not null default N'Chưa đặt tên',			-- Tên bàn
+	StatusID int not null  default 0,								-- Trạng thái bàn
+	TableType int not null default 1,								-- Loại bàn	
+	
+	check (TableType > 0),
+	foreign key (StatusID) references TableStatus(ID)
+)
+go
+
+-- Thức ăn
+create table Foods
+(
+	ID int identity primary key,									-- Mã
+	[Name] nvarchar(100) not null default N'Chưa đặt tên',			-- Tên thức ăn
+	CategoryID int not null,										-- Mã loại thức ăn
+	Price int not null default 0,
+	
+	foreign key (CategoryID) references FoodCategory(ID)
+)
+go
+
+-- Hoá đơn
+create table Bills
+(
+	ID int identity primary key,									-- Mã
+	CheckIn datetime not null default getDate(),					-- Thời gian vào
+	CheckOut datetime,												-- Thời gian ra
+	TableID int not null,											-- Mã bàn ăn
+	StatusID int not null default 0,								-- Trạng thái hoá đơn
+
+	check(CheckIn <= CheckOut),
+	foreign key (TableID) references FoodTables(ID),
+	foreign key (StatusID) references BillStatus(ID)
+)
+go
+
+-- Chi tiết hoá đơn
+create table BillDetail
+(
+	ID int identity primary key,									-- Mã
+	BillID int not null references Bills(ID),						-- Mã hoá đơn
+	FoodID int not null references Foods(ID),						-- Mã thức ăn
+	Quantity int not null default 0,
+
+	foreign key (BillID) references Bills(ID),						
+	foreign key (FoodID) references Foods(ID)	
+)
+go
